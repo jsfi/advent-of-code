4528fad4b52327e7578594d38ae97a781baefcab
Martin Göttsche <martin.sachse+jsfi@gmail.com>
2023-12-07 21:09:21 +0100

    :sparkles: solve day 7

---
+++ day07/star1.js
@@ -0,0 +1,20 @@
+import {getHandCount, getHandsWithBets, getRankingHands} from './shared.js';
+
+const handsWithBets = await getHandsWithBets();
+
+const [addHandWithBetByCounts, getWinnings] = getRankingHands([
+    '2', '3', '4', '5',
+    '6', '7', '8', '9',
+    'T', 'J', 'Q', 'K', 'A'
+]);
+
+for (const handWithBet of handsWithBets) {
+    const [orderedCounts] = getHandCount(handWithBet[0]);
+    const [highestCount, secondHighestCount] = orderedCounts;
+
+    addHandWithBetByCounts(handWithBet, highestCount, secondHighestCount);
+}
+
+const winnings = getWinnings();
+
+console.log(winnings);
---
+++ day07/star2.js
@@ -0,0 +1,31 @@
+import {getHandCount, getHandsWithBets, getRankingHands} from './shared.js';
+
+const handsWithBets = await getHandsWithBets();
+
+const [addHandWithBetByCounts, getWinnings] = getRankingHands([
+    'J',
+    '2', '3', '4', '5',
+    '6', '7', '8', '9',
+    'T', 'Q', 'K', 'A'
+]);
+
+for (const handWithBet of handsWithBets) {
+    const [orderedCounts, handCount] = getHandCount(handWithBet[0]);
+    let [highestCount, secondHighestCount] = orderedCounts;
+
+    const jokerCount = handCount.get('J');
+    if (jokerCount) {
+        if (jokerCount === 5) {
+            // do nothing
+        } else if (jokerCount === highestCount || jokerCount === secondHighestCount) {
+            highestCount += secondHighestCount;
+            secondHighestCount = orderedCounts[2];
+        } else {
+            highestCount += jokerCount;
+        }
+    }
+
+    addHandWithBetByCounts(handWithBet, highestCount, secondHighestCount);
+}
+
+console.log(getWinnings());
