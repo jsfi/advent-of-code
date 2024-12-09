f1ac6d05661e637ac5952f9c2d08dea7f3e72262
Martin Göttsche <martin.sachse+jsfi@gmail.com>
2024-12-09 20:17:43 +0100

    :sparkles: Solve day 09

---
+++ day09/star1.ts
@@ -0,0 +1,50 @@
+import { getLines } from '../get-lines.ts';
+
+const line = getLines(import.meta.url)[0];
+
+let position = 0;
+const spaces: Array<number> = [];
+const files: Array<{ id: number, positions: Array<number> }> = [];
+for (let i = 0; i < line.length; i++) {
+	const size = Number(line[i]);
+
+	if (i % 2 === 0) {
+		const filePositions = [];
+		for (let j = 0; j < size; j++) {
+			filePositions.push(position++);
+		}
+		files.push({ id: Math.floor(i / 2), positions: filePositions });
+	} else {
+		for (let j = 0; j < size; j++) {
+			spaces.push(position++);
+		}
+	}
+}
+
+let fileIndex = files.length - 1;
+let positionIndex = files[fileIndex].positions.length - 1;
+while (spaces.length) {
+	if (positionIndex === -1) {
+		fileIndex--;
+		if (fileIndex === -1) {
+			break;
+		}
+		positionIndex = files[fileIndex].positions.length - 1;
+		continue;
+	}
+
+	const space = spaces.shift()!;
+	const currentPosition = files[fileIndex].positions[positionIndex];
+	if (currentPosition < space) {
+		break;
+	}
+
+	files[fileIndex].positions[positionIndex] = space;
+	positionIndex--;
+}
+
+const result = files.reduce((checksum, { id, positions }) => {
+	const checkSumFile = positions.reduce((checkSumFile, position) => checkSumFile + (id * position), 0);
+	return checksum + checkSumFile;
+}, 0);
+console.log(result);
---
+++ day09/star2.ts
@@ -0,0 +1,53 @@
+import { getLines } from '../get-lines.ts';
+
+const line = getLines(import.meta.url)[0];
+
+let position = 0;
+const spaces: Array<{ from: number, size: number }> = [];
+const files: Array<{ id: number, from: number, size: number }> = [];
+for (let i = 0; i < line.length; i++) {
+	const size = Number(line[i]);
+
+	if (i % 2 === 0) {
+		files.push({ id: Math.floor(i / 2), from: position, size });
+	} else {
+		spaces.push({ from: position, size });
+	}
+
+	position += size;
+}
+
+for (let fileIndex = files.length - 1; fileIndex >= 0; fileIndex--) {
+	const file = files[fileIndex];
+	const fileSize = file.size;
+
+	for (let spaceIndex = 0; spaceIndex < spaces.length; spaceIndex++) {
+		const space = spaces[spaceIndex];
+
+		if (space.from >= file.from) {
+			break;
+		}
+
+		if (space.size < fileSize) {
+			continue;
+		}
+
+		file.from = space.from;
+
+		if (space.size === fileSize) {
+			spaces.splice(spaceIndex, 1);
+		} else {
+			space.from += fileSize;
+			space.size -= fileSize;
+		}
+	}
+}
+
+const result = files.reduce((checksum, { id, from, size }) => {
+	let checkSumFile = 0;
+	for (let i = 0; i < size; i++) {
+		checkSumFile += id * (from + i);
+	}
+	return checksum + checkSumFile;
+}, 0);
+console.log(result);
