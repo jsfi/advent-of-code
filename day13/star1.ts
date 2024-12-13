8d3b8d96d0000502762902d393427bf88e0327d5
Martin Göttsche <martin.sachse+jsfi@gmail.com>
2024-12-13 18:00:56 +0100

    :sparkles: Solve day 13

---
+++ day13/star1.ts
@@ -0,0 +1,59 @@
+import { getLines } from '../get-lines.ts';
+
+const lines = getLines(import.meta.url);
+
+const COST_A = 3;
+const COST_B = 1;
+const MAX = 100;
+
+const machines = [];
+for (let i = 0; i < lines.length; i += 4) {
+	const matchA = /X\+(?<x>\d+), Y\+(?<y>\d+)/.exec(lines[i])?.groups!;
+	const matchB = /X\+(?<x>\d+), Y\+(?<y>\d+)/.exec(lines[i+1])?.groups!;
+	const prize = /X=(?<x>\d+), Y=(?<y>\d+)/.exec(lines[i+2])?.groups!;
+
+	machines.push({
+		a: {
+			x: Number(matchA.x),
+			y: Number(matchA.y),
+		},
+		b: {
+			x: Number(matchB.x),
+			y: Number(matchB.y),
+		},
+		prize: {
+			x: Number(prize.x),
+			y: Number(prize.y),
+		},
+	})
+}
+
+let cost = 0;
+for (const machine of machines) {
+	let minCost = Infinity;
+	for (let a = 0; a < MAX; a++) {
+		const costA = a * COST_A;
+		if (costA > minCost) {
+			break;
+		}
+
+		for (let b = 0; b < MAX; b++) {
+			if (
+				machine.prize.x === machine.a.x * a + machine.b.x * b &&
+				machine.prize.y === machine.a.y * a + machine.b.y * b
+			) {
+				const costRound = costA + b * COST_B;
+				if (costRound < minCost) {
+					minCost = costRound;
+					break;
+				}
+			}
+		}
+	}
+
+	if (minCost !== Infinity) {
+		cost += minCost;
+	}
+}
+
+console.log(cost);
---
+++ day13/star2.ts
@@ -0,0 +1,52 @@
+import { getLines } from '../get-lines.ts';
+
+const lines = getLines(import.meta.url);
+
+const COST_A = 3;
+const COST_B = 1;
+
+const machines = [];
+for (let i = 0; i < lines.length; i += 4) {
+	const matchA = /X\+(?<x>\d+), Y\+(?<y>\d+)/.exec(lines[i])?.groups!;
+	const matchB = /X\+(?<x>\d+), Y\+(?<y>\d+)/.exec(lines[i+1])?.groups!;
+	const prize = /X=(?<x>\d+), Y=(?<y>\d+)/.exec(lines[i+2])?.groups!;
+
+	machines.push({
+		a: {
+			x: Number(matchA.x),
+			y: Number(matchA.y),
+		},
+		b: {
+			x: Number(matchB.x),
+			y: Number(matchB.y),
+		},
+		prize: {
+			x: Number(prize.x) + 10000000000000,
+			y: Number(prize.y) + 10000000000000,
+		},
+	})
+}
+
+/**
+ * PrizeX = A * AX + B * BX
+ * PrizeY = A * AY + B * BY
+ *  ->
+ * A = (PrizeX - B * BX) / AX
+ * A = (PrizeY - B * BY) / AY
+ *  ->
+ * (PrizeX - B * BX) / AX = (PrizeY - B * BY) / AY
+ *  ->
+ * B = (AX * PrizeY - AY * PrizeX) / (AX * BY - AY * BX)
+ */
+
+let result = 0;
+for (const machine of machines) {
+	const b = (machine.a.x * machine.prize.y - machine.a.y * machine.prize.x) / (machine.a.x * machine.b.y - machine.a.y * machine.b.x);
+	const a = (machine.prize.x - b * machine.b.x) / machine.a.x;
+
+	if (a === Math.floor(a) && b === Math.floor(b)) {
+		result += a * COST_A + b * COST_B;
+	}
+}
+
+console.log(result);
